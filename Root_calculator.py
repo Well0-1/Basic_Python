a45a2e3e573da040dc46deca63d387480106acea
Salih Akyüzoğlu <88163696+Well0-1@users.noreply.github.com>
2023-12-18 21:04:55 +0300

    Add files via upload

---
+++ Root_calculator.py
@@ -0,0 +1,19 @@
+def root():
+    delta = b**2 - 4*a*c
+    if delta < 0 :
+        print("Denklemin Kökü Yoktur")
+        
+    elif delta == 0 :
+        print("Denkleminiz Çift Katlı Köktür!")
+        print(-b/2*a)
+    
+    elif delta > 0 :
+        print("1.Kök = " , (-b + delta**0.5)/2*a )
+        print("2.Kök = " , (-b - delta**0.5)/2*a )
+
+print("Lüften ax^2 + bx + c Şeklinde olan formülün a,b ve c değerlerini giriniz")
+a = int(input("a'yi giriniz: "))
+b = int(input("b'yi giriniz: "))
+c = int(input("c'yi giriniz: "))
+
+sonuc = root()
