aa2cdd77f947608e6ae69bbb47cf1881818b50f6
Salih Akyüzoğlu <88163696+Well0-1@users.noreply.github.com>
2023-12-25 22:19:06 +0300

    Update CheckReg.py

--- CheckReg.py
+++ CheckReg.py
@@ -1,38 +1,54 @@
-students = {"2006": {
-                'Adi': "Salih",
-                'Soyadi': "Akyüzoğlu",
-                'Tel No': "555-5555" },
-            "478": {
-                'Adi': "Muhammet",
-                'Soyadi': "Akyüzoğlu",
-                'Tel No': "521-1942" },
-            "1223": {
-                'Adi': "Yavuz Selim",
-                'Soyadi': "Akyüzoğlu",
-                'Tel No': "541-0581" },      
-            "2006": {
-                'Adi': "Muhammet Emin",
-                'Soyadi': "Genç",
-                'Tel No': "559-4808" }    
-            }
+students = {}
 
-while True:
-    check = input("Kontrol Edilecek Öğrenci No: ")
-    if check in students :
-        print(f"{check} Numaralı Öğrenci Bilgileri :")
-        for k, v in students[check].items() :
-            print(f"{k}: {v}")
-        loop = input("Farklı Bir Öğrenci Kontrol Etmek İstiyor musunuz? (E/H) ").upper()
-        if loop == "E":
-            continue
-        else :
-            print("Program Kapanıyor...")
+# Before I added the other functions
+def check_in_terminal():
+    while True:
+        check = input("Kontrol Edilecek Öğrenci No: ")
+        if check in students :
+            print(f"{check} Numaralı Öğrenci Bilgileri :")
+            for key, value in students[check].items() :
+                print(f"{key}: {value}")
+            loop = input("Farklı Bir Öğrenci Kontrol Etmek İstiyor musunuz? (E/H) ").upper()
+            if loop == "E":
+                continue
+            else :
+                print("Program Kapanıyor...")
+                break
+        elif not check in students :
+            print("Böyle Bir Öğrenci Bulunmamaktadır")
+            loop = input("Farklı Bir Öğrenci Kontrol Etmek İstiyor musunuz? (E/H) ").upper()
+            if loop == "E":
+                continue
+            else : 
+                print("Program Kapanıyor...")
+                break
+#
+
+def read_from_file() :
+    with open("students_info.txt","r") as file :
+        lines = file.readlines()
+        for line in lines :
+            data = line.strip().split(",")
+            number = data[0]
+            name = data[1]
+            lname = data[2]
+            phone = data[3]
+            students[number] = {"Adi": name, "Soyadi": lname, "Tel No" : phone}  
+    return students
+            
+def check_info() :
+    while True:
+        check = input("Kontrol Edilecek Öğrenci No: ")    
+        
+        if check in students:
+            print(f"{check} Numaralı Öğrenci Bilgileri :")
+            for key, value in students[check].items():
+                print(f"{key}: {value}")
             break
-    elif not check in students :
-        print("Böyle Bir Öğrenci Bulunmamaktadır")
-        loop = input("Farklı Bir Öğrenci Kontrol Etmek İstiyor musunuz? (E/H) ").upper()
-        if loop == "E":
+        else:  
+            print("Böyle Bir Öğrenci Bulunmamaktadır")
             continue
-        else : 
-            print("Program Kapanıyor...")
-            break
+
+read_from_file()
+
+check_info()
