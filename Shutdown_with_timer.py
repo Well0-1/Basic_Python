08d2b8d38c50d34dedf2d3db13dd663af6ffed4f
Salih Akyüzoğlu <88163696+Well0-1@users.noreply.github.com>
2024-01-02 11:34:15 +0300

    Update Shutdown_with_timer.py

--- Shutdown_with_timer.py
+++ Shutdown_with_timer.py
@@ -1,7 +1,9 @@
 import time
 import os
 
-sleep_time = int(input("Saniye Cinsinden Bilgisayar kaç saniye sonra kapanmasini istiyorsunuz ? : "))
+# Works With Seconds
+
+sleep_time = int(input("Saniye Cinsinden Bilgisayar kaç saniye sonra kapanmasini istiyorsunuz ? : ")) # Shuts down the computer after as many seconds as the number you enter
 
 for i in range(sleep_time) :
     timer = sleep_time - i 
@@ -9,4 +11,26 @@ for i in range(sleep_time) :
     if timer <= 30 :
         print(timer)
                 
+os.system("shutdown /s /t 1")
+
+# Minute Version
+
+import time
+import os
+
+sleep_time = input("Bilgisayarınız Kaç Dakika Sonra Kapatılsın ? : ") # Shuts down the computer after as many minutes as the number you enter
+
+while not sleep_time.isdigit() :
+    print("Bir Tam Sayı Girmeniz Gerekmektedir!")
+    sleep_time = input("Bilgisayarınız Kaç Dakika Sonra Kapatılsın ? : ")
+
+sleep_time = int(sleep_time)
+
+for i in range(sleep_time) :
+    timer = sleep_time - i 
+    if timer <= 5 :
+        print(f"Bilgisayarınız {timer} dakika sonra kapanacaktır!")
+    time.sleep(60)
+
+print("Shutting Down...")
 os.system("shutdown /s /t 1")
