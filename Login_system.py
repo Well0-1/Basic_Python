72bd44693c127d74c471cbdb61329ad18313fd5d
Salih Akyüzoğlu <88163696+Well0-1@users.noreply.github.com>
2024-01-06 16:51:22 +0300

    Update Login_system.py

--- Login_system.py
+++ Login_system.py
@@ -4,25 +4,62 @@ accounts = {"Admin":"12345",
             "User3":"cantfindany",
             }
 
+    
+def signup() :
+    ask_signup = input("Kayıt Olmak İçin 'E', Tekrar Denemek İçin 'H', Programı Kapatmak İçin 'Q' Tuşlayınız (E/H/Q) : ").upper()
+    
+    if ask_signup == "E" :
+        print("-------------------------------------------Kayıt Programı ----------------------------------------------------------")
+        while True :
+            new_user = input("Kullanıcı Adı: ")
+            for i in accounts :
+                while new_user == i :
+                        print("Girilen Kullanıcı Adı Halihazırda Kullanımdadır Lütfen Farklı Bir Kullanıcı Adı Seçiniz!")
+                        new_user = input("Kullanıcı Adı: ")
+
+            new_pass = input("Şifre: ") 
+            accounts.update({new_user:new_pass})
+            print(f"Sayın Kullanıcı {new_user}, Kaydınız Başarıyla Gerçekleşmiştir!\nPrograma Yönlendiriliyorsunuz...")    
+            return True
+    elif ask_signup == "H" :
+        return False
+    else : 
+        print("Program Kapatılıyor...")
+        exit()    
+
+    
 def login() :
-    trys = 10
+    trys = 0
+    
     while True : 
+        
         username = input("Kullanıcı Adı : ")
         password = input("Şifre : ")
+    
+        if not username in accounts :
+            print("Böyle Bir Kullanıcı Bulunmamaktadır!")
+            break
+    
         for k, v in accounts.items() :
             if username == k and password == v :
                 print("Giriş Başarılı!")
-                return False
+                return True
             else :
                 continue
-        print("Geçersiz Kullanıcı Adı Veya Şifre Lütfen Tekrar Deneyiniz!")
-        trys -= 1
-        if trys == 0 :
+        print("Hatalı Kullanıcı Adı Veya Şifre")        
+        
+        trys += 1
+        if trys == 5 :
             print("Çok Sayıda Yanlış Deneme Yapıldı Program Kapatılıyor!")
             exit()
-login()
+
+while True :
+    if login() == True :
+        exit()
+    signup()
+
+
 
 # add-list
-# signup **kwargs 
 # import data from txt file 
 # change password 
