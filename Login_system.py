93e9b1e6145dd6f8d1dee9931ba4ddf27a4b3719
Salih Akyüzoğlu <88163696+Well0-1@users.noreply.github.com>
2024-01-07 18:34:24 +0300

    Update Login_system.py

--- Login_system.py
+++ Login_system.py
@@ -6,60 +6,60 @@ accounts = {"Admin":"12345",
 
     
 def signup() :
-    ask_signup = input("Kayıt Olmak İçin 'E', Tekrar Denemek İçin 'H', Programı Kapatmak İçin 'Q' Tuşlayınız (E/H/Q) : ").upper()
-    
-    if ask_signup == "E" :
-        print("-------------------------------------------Kayıt Programı ----------------------------------------------------------")
-        while True :
-            new_user = input("Kullanıcı Adı: ")
-            for i in accounts :
-                while new_user == i :
-                        print("Girilen Kullanıcı Adı Halihazırda Kullanımdadır Lütfen Farklı Bir Kullanıcı Adı Seçiniz!")
-                        new_user = input("Kullanıcı Adı: ")
+    print("-------------------------------------------Kayıt Programı ----------------------------------------------------------")
+    while True :
+        new_user = input("Kullanıcı Adı: ")
+        #for i in accounts :
+        #    while new_user == i :
+        #            print("Girilen Kullanıcı Adı Halihazırda Kullanımdadır, Lütfen Farklı Bir Kullanıcı Adı Seçiniz!")
+        #            new_user = input("Kullanıcı Adı: ")
+        new_pass = input("Şifre: ") 
+        
+        accounts.update({new_user:new_pass})
+        print(f"Sayın Kullanıcı {new_user}, Kaydınız Başarıyla Gerçekleşmiştir!\nPrograma Yönlendiriliyorsunuz...")    
+        return True
 
-            new_pass = input("Şifre: ") 
-            accounts.update({new_user:new_pass})
-            print(f"Sayın Kullanıcı {new_user}, Kaydınız Başarıyla Gerçekleşmiştir!\nPrograma Yönlendiriliyorsunuz...")    
-            return True
-    elif ask_signup == "H" :
-        return False
-    else : 
-        print("Program Kapatılıyor...")
-        exit()    
-
-    
 def login() :
     trys = 0
-    
+    wrong_pass = 0
     while True : 
         
         username = input("Kullanıcı Adı : ")
         password = input("Şifre : ")
-    
-        if not username in accounts :
-            print("Böyle Bir Kullanıcı Bulunmamaktadır!")
-            break
-    
+        
         for k, v in accounts.items() :
             if username == k and password == v :
                 print("Giriş Başarılı!")
                 return True
+            elif username == k and password != v :
+                print("Hatalı Kullanıcı Adı Veya Şifre")
+                wrong_pass += 1
+                if wrong_pass == 3 :
+                    ask_change = input("Şifrenizi Mi Unuttunuz? (E/H): ").upper()
+                    if ask_change == "E":
+                        return "Incorrect"
+            elif username not in accounts :
+                print("Böyle Bir Kullanıcı Bulunmamaktadır")
+                ask_signup = input("Kayıt Olmak İçin 'E' Tekrar Denemek İçin 'H' Tuşlayınız: ").upper()
+                if ask_signup == "E":
+                    return "Signup"
+                else :
+                    return False
             else :
-                continue
-        print("Hatalı Kullanıcı Adı Veya Şifre")        
-        
+                continue   
         trys += 1
         if trys == 5 :
             print("Çok Sayıda Yanlış Deneme Yapıldı Program Kapatılıyor!")
             exit()
 
+def changepass() :
+    print("Working on it")
+
 while True :
-    if login() == True :
+    login_result = login()
+    if login_result == True :
         exit()
-    signup()
-
-
-
-# add-list
-# import data from txt file 
-# change password 
+    elif login_result == "Incorrect" :
+        changepass()
+    elif login_result == "Signup" :
+        signup()
