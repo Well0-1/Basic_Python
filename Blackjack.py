6b051b0ddd4cbae5bb57727354684f3a84db85f7
Salih Akyüzoğlu <88163696+Well0-1@users.noreply.github.com>
2024-02-18 23:41:57 +0300

    Update Blackjack.py

--- Blackjack.py
+++ Blackjack.py
@@ -16,10 +16,7 @@ values = {
     "Q": 10,
     "J": 10,
 }
-
-
 Deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"] * 4
-
 Dealer.shuffle(Deck)
 
 
@@ -78,12 +75,10 @@ def game():
 
     print(f"Your Hand = {player_hand}\nDealer's Hand = {dealer_hand}")
 
-    choice = input("Do You Want to Hit or Stay?").capitalize()
+    choice = input("Do You Want to Hit or Stay?").capitalize() # İf your Hand is like ['A'and any of these cards[Q,K,J,10]] It doesnt automatically understands that you have blackjack so you have to stay.Sorry too lazy to do it
     while not choice in ["H", "S", "Stay", "Hit"]:
         print("You Have to Enter Your Move!")
-        choice = input(
-            "Do You Want to Hit or Stay? İf You Want to Quit Enter 'Q'"
-        ).capitalize()
+        choice = input("Do You Want to Hit or Stay? İf You Want to Quit Enter 'Q': ").capitalize()
         if choice == "Q":
             print("Have a Nice Day!")
             quit()
@@ -98,9 +93,7 @@ def game():
             print("Blackjack! Waiting For The Dealer...")
             dealer_hand[0] = hidden_card
             wait(0.5)
-            print(
-                f"Dealer Reveals the Hidden Card... It's {hidden_card}\nDealer's Hand = {dealer_hand}"
-            )
+            print(f"Dealer Reveals the Hidden Card... It's {hidden_card}\nDealer's Hand = {dealer_hand}")
             dealer_point = dealer_p(dealer_hand)
             while dealer_point < user_point:
                 wait(1)
@@ -115,15 +108,12 @@ def game():
                 print("Draw!")
                 quit()
             elif dealer_point > 21:
-                print("You Won!")
                 quit()
         elif len(player_hand) == 5 and user_point <= 21:
             print("5 Card Trick!")
             while not len(dealer_hand) == 5:
                 dealer_hand[0] = hidden_card
-                print(
-                    f"Dealer Reveals the Hidden Card... It's {hidden_card}\nDealer's Hand = {dealer_hand}"
-                )
+                print(f"Dealer Reveals the Hidden Card... It's {hidden_card}\nDealer's Hand = {dealer_hand}")
                 wait(1)
                 card = Deck.pop()
                 print(f"Dealer Hits... And Card is {card}")
@@ -133,25 +123,18 @@ def game():
                 wait(0.5)
                 dealer_point = dealer_p(dealer_hand)
                 if dealer_point > 21:
-                    print("You Won")
                     quit()
             if dealer_point > user_point:
-                print(
-                    "Dealer Wins! My Advice, You Really Should Check This Website https://quickabdest.com"
-                )
+                print("Dealer Wins! My Advice, You Really Should Check This Website https://quickabdest.com")
                 quit()
             elif dealer_point == user_point:
-                print(
-                    "Draw! My Advice, You Really Should Check This Website https://quickabdest.com"
-                )
+                print("Draw! My Advice, You Really Should Check This Website https://quickabdest.com")
         choice = input("Do You Want to Hit or Stay?").capitalize()
 
     if choice in ["S", "Stay"]:
         user_point = point(player_hand)
         dealer_hand[0] = hidden_card
-        print(
-            f"Dealer Reveals the Hidden Card... It's {hidden_card}\nDealer's Hand = {dealer_hand}"
-        )
+        print(f"Dealer Reveals the Hidden Card... It's {hidden_card}\nDealer's Hand = {dealer_hand}")
         dealer_point = dealer_p(dealer_hand)
         if dealer_point > user_point:
             print("Dealer Won!")
@@ -172,6 +155,5 @@ def game():
             elif dealer_point == user_point:
                 print("Draw!")
                 quit()
-
 
 game()
